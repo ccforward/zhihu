441c352495bc1e6a59387d5e6677636f9a7a610c
cc <ccking@foxmail.com>
2015-07-20 15:42:13 +0800

    缓存请求

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+//
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- public/p/test/index.js
+++ public/p/test/index.js
@@ -3,6 +3,9 @@
     calendar.init();
 
     var Daily = {
+        _CONSTDATA: {
+            history: {}
+        },
         init: function(){
             this.events();
         },
@@ -14,18 +17,31 @@
             });
         },
         zhData: function(date){
-            $.ajax({
-                type: 'GET',
-                url: '/d/'+date,
-                success: function(data){
-                    var d = JSON.parse(data);
-                    var list = '';
-                    for(var i=0,len=d.length;i<len;i++){
-                        list += '<p>'+d[i].title+'</p>';
+            var _self = this;
+            if(_self._CONSTDATA.history[date]){
+                _self.render(_self._CONSTDATA.history[date]);
+            }else {
+                $.ajax({
+                    type: 'GET',
+                    url: '/d/'+date,
+                    success: function(data){
+                        // 存储已请求内容
+                        var d = JSON.parse(data);
+                        if(!_self._CONSTDATA.history[date]){
+                            _self._CONSTDATA.history[date] = d;
+                        }
+                        _self._render(d);
                     }
-                    $('.right-list').html(list);
-                }
-            })
+                });
+
+            }
+        },
+        _render: function(d){
+            var list = '';
+            for(var i=0,len=d.length;i<len;i++){
+                list += '<p>'+d[i].title+'</p>';
+            }
+            $('.right-list').html(list);            
         }
 
     }
