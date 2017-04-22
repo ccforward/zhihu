296985ca479d1a6db9e38cdec539e0486ccaf9ab
cc <ccking@foxmail.com>
2017-04-22 23:31:26 +0800

    update dev:fe command

--- app.js
+++ app.js
@@ -15,35 +15,34 @@ const routes = require('./routes/index');
 const app = express();
 
 // webpack
-if(CONFIG.fe.developing){
-    require("nodejs-dashboard");
-    var webpackConfig = require('./build/webpack.dev.conf');
-    var webpack = require('webpack');
-    var webpackDevMiddleware = require('webpack-dev-middleware');
-    var compiler = webpack(webpackConfig);
-    var devMiddleware = webpackDevMiddleware(compiler, {
-        publicPath: webpackConfig.output.publicPath,
-        stats: {
-            colors: true,
-            chunks: true,
-            progress: true
-        }
-    });
-    var hotMiddleware = require('webpack-hot-middleware')(compiler);
-    app.use(devMiddleware);
-    app.use(hotMiddleware);
+if (process.argv[2] == '-fe') {
+  var webpackConfig = require('./build/webpack.dev.conf');
+  var webpack = require('webpack');
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var compiler = webpack(webpackConfig);
+  var devMiddleware = webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: true,
+      progress: true
+    }
+  });
+  var hotMiddleware = require('webpack-hot-middleware')(compiler);
+  app.use(devMiddleware);
+  app.use(hotMiddleware);
 }
 
 // 爬虫任务
 const Job = require('./spider/util/task');
 const SpiderMan = require('./spider/spider');
 
-if(CONFIG.spider.fire) {
-    SpiderMan.fire(CONFIG.spider.start, CONFIG.spider.end);
+if (CONFIG.spider.fire) {
+  SpiderMan.fire(CONFIG.spider.start, CONFIG.spider.end);
 }
-if(CONFIG.spider.openTask) {
-    SpiderMan.latest();
-    Job.fire();
+if (CONFIG.spider.openTask) {
+  SpiderMan.latest();
+  Job.fire();
 }
 
 // 生成统计数据
@@ -67,63 +66,63 @@ app.use('/', routes);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    // next(err);
-    res.render('error', {
-        message: err.message,
-        error: err
-    });
+  var err = new Error('Not Found');
+  err.status = 404;
+  // next(err);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
 // ============== log4js init ==============
-if(CONFIG.log.isOpenningNode){
-    var log4js = require('log4js');
-    log4js.loadAppender('file');
-    log4js.configure({
-        appenders: [
-            { type: 'console' },
-            { type: 'file', filename: './log/cheese.log', category: 'cheese' }
-        ]
-    });
+if (CONFIG.log.isOpenningNode) {
+  var log4js = require('log4js');
+  log4js.loadAppender('file');
+  log4js.configure({
+    appenders: [
+      { type: 'console' },
+      { type: 'file', filename: './log/cheese.log', category: 'cheese' }
+    ]
+  });
 }
 // ============== log4js init ==============
 
 
 // ============== HTTP log ==============
-if(CONFIG.log.isOpenningHTTP){
-    var logDirectory = path.join(__dirname, 'log');
-    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-    var accessLogStream = FileStreamRotator.getStream({
-        date_format: 'YYYYMMDD',
-        filename: path.join(logDirectory, 'access-%DATE%.log'),
-        frequency: 'daily',
-        verbose: false
-    });
-    app.use(morgan('combined', { stream: accessLogStream }));
+if (CONFIG.log.isOpenningHTTP) {
+  var logDirectory = path.join(__dirname, 'log');
+  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+  var accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDirectory, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+  });
+  app.use(morgan('combined', { stream: accessLogStream }));
 }
 // ============== HTTP log ==============
 
